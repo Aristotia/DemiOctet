65285e2cc158909b8bd79cc775225fbb82c56277
MidNightRoad <bergerpierre09@gmail.com>
2022-06-29 13:46:36 +0200

    test

--- frontend/src/pages/Connection.jsx
+++ frontend/src/pages/Connection.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import BurgerMenu from "../components/BurgerMenu";
 
 function Connection() {
   return (
     <div className="homecontainer">
-      <BurgerMenu />
-
       <div className="leftside">coucou</div>
       <div className="rightside"> lol</div>
     </div>
