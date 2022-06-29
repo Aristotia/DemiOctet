4d4e35d2f329a2e624e4b873a4b505d7549d8695
Aristotia <antoine.belloni@gmail.com>
2022-06-29 15:48:57 +0200

    basic routes created for user, agency, task, collaborator and project table; task table created

--- backend/database.sql
+++ backend/database.sql
@@ -28,7 +28,7 @@ CREATE TABLE `agency` (
   `address` text,
   `phone_number` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `agency` (
 
 LOCK TABLES `agency` WRITE;
 /*!40000 ALTER TABLE `agency` DISABLE KEYS */;
-INSERT INTO `agency` VALUES (1,'Paris','58, avenue du Général Leclerc, 92514 Boulogne Cedex','(+33) 1 41 10 01 61'),(2,'Nantes','1 Rue Haute Casserie, 44000 Nantes','(+33) 2 40 35 67 89'),(3,'Rennes','24, rue de l\'Alma, Immeuble Urban Quartz, 35000 Rennes','(+33) 2 99 67 00 22'),(4,'Aix-En-Provence','7 Route de Galice, 13100 Aix-en-Provence','(+33) 4 42 93 71 01'),(5,'Bordeaux','23 quai de Paludate - 33800 Bordeauxe','(+33) 5 57 59 02 50'),(6,'Genève','Rue du Marché 3, 1201 Genève, Suisse','(+41) 22 400 00 34'),(7,'Lyon','79 Bd Marius Vivier Merle, 69003 Lyon',' (+33) 4 37 48 31 30'),(8,'Munich','Konrad-Zuse-Platz 8, 81829 München','+49 89 20 70 42 100'),(9,'Niort','7 rue du Docteur Roux, 79000 Niort','06 35 52 79 49'),(10,'Montréal','407, rue McGill, suite 700 Montréal (QC) H2Y 2G3','+1 438 922 1299'),(11,'Brest','90 Rue Ernest Hemingway, 29200 Brest','(+33) 2 98 44 67 86'),(12,'Dijon','10 Avenue Maréchal Foch, 21000 Dijon','(+33) 7 63 62 89 35'),(13,'Lille','100 Rue Nationale, 59000 Lille','(+33) 6 40 81 20 33'),(14,'Lille','100 Rue Nationale, 59000 Lille','(+33) 3 20 79 83 30'),(15,'Clermont-Ferrand','17 rue de Sarliève, 63800 Cournon d’Auvergne','(+33) 4 73 28 57 20'),(16,'Tours','12 rue Fabienne Landy, 37700 St Pierre des Corps','(+33) 2 47 46 26 26'),(17,'Nice - Sophia Antipolis','400 avenue Roumanille, 06906 Sophia Antipolis','(+33) 4 93 00 11 05'),(18,'Strasbourg','5 allée Cérès, 67200 Strasbourg','(+33) 9 72 12 49 44'),(19,'Toulouse','21 Bd de la Marquette, 31000 Toulouse','(+33)5 61 62 02 03'),(20,'Bruxelles','Avenue des Arts, 46, 1000 Bruxelles - Belgium','(+32) 2 319 34 45'),(21,'Orléans','6 bis avenue Jean Zay, 45000 Orléans','(+33) 2 38 77 12 61'),(22,'Casablanca','203 BD ABDELMOUMEN ET BD ANOUAL, 20042 CASABLANCA','+212 5 29 05 48 67'),(23,'Le Mans','39 boulevard Demorieux R+4 sud Bat epsilon 72100 Le Mans','(+33) 2 47 46 26 26'),(24,'Aveiro','Av. Europa n.º 404, 1º Dto, 3800-230 Cacia Aveiro','(+351) 9 10 24 07 52');
+INSERT INTO `agency` VALUES (1,'Paris','58, avenue du Général Leclerc, 92514 Boulogne Cedex','(+33) 1 41 10 01 61'),(3,'Rennes','24, rue de l\'Alma, Immeuble Urban Quartz, 35000 Rennes','(+33) 2 99 67 00 22'),(4,'Aix-En-Provence','7 Route de Galice, 13100 Aix-en-Provence','(+33) 4 42 93 71 01'),(5,'Bordeaux','23 quai de Paludate - 33800 Bordeauxe','(+33) 5 57 59 02 50'),(6,'Genève','Rue du Marché 3, 1201 Genève, Suisse','(+41) 22 400 00 34'),(7,'Lyon','79 Bd Marius Vivier Merle, 69003 Lyon',' (+33) 4 37 48 31 30'),(8,'Munich','Konrad-Zuse-Platz 8, 81829 München','+49 89 20 70 42 100'),(9,'Niort','7 rue du Docteur Roux, 79000 Niort','06 35 52 79 49'),(10,'Montréal','407, rue McGill, suite 700 Montréal (QC) H2Y 2G3','+1 438 922 1299'),(11,'Brest','90 Rue Ernest Hemingway, 29200 Brest','(+33) 2 98 44 67 86'),(12,'Dijon','10 Avenue Maréchal Foch, 21000 Dijon','(+33) 7 63 62 89 35'),(14,'Lille','100 Rue Nationale, 59000 Lille','(+33) 3 20 79 83 30'),(15,'Clermont-Ferrand','17 rue de Sarliève, 63800 Cournon d’Auvergne','(+33) 4 73 28 57 20'),(16,'Tours','12 rue Fabienne Landy, 37700 St Pierre des Corps','(+33) 2 47 46 26 26'),(17,'Nice - Sophia Antipolis','400 avenue Roumanille, 06906 Sophia Antipolis','(+33) 4 93 00 11 05'),(18,'Strasbourg','5 allée Cérès, 67200 Strasbourg','(+33) 9 72 12 49 44'),(19,'Toulouse','21 Bd de la Marquette, 31000 Toulouse','(+33)5 61 62 02 03'),(20,'Bruxelles','Avenue des Arts, 46, 1000 Bruxelles - Belgium','(+32) 2 319 34 45'),(21,'Orléans','6 bis avenue Jean Zay, 45000 Orléans','(+33) 2 38 77 12 61'),(22,'Casablanca','203 BD ABDELMOUMEN ET BD ANOUAL, 20042 CASABLANCA','+212 5 29 05 48 67'),(23,'Le Mans','39 boulevard Demorieux R+4 sud Bat epsilon 72100 Le Mans','(+33) 2 47 46 26 26'),(24,'Aveiro','Av. Europa n.º 404, 1º Dto, 3800-230 Cacia Aveiro','(+351) 9 10 24 07 52'),(27,'Tourcoing','27, rue de la Recette','06.27.27.27.27'),(28,'Nantes','1 Rue Haute Casserie, 44000 Nantes','(+33) 2 40 35 67 89');
 /*!40000 ALTER TABLE `agency` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,8 +50,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:42:25
-
+-- Dump completed on 2022-06-29 15:45:07
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -105,7 +104,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:10:31
+-- Dump completed on 2022-06-29 15:45:09
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -139,7 +138,7 @@ CREATE TABLE `collaborator` (
   `email` varchar(255) DEFAULT NULL,
   `phone_number` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,6 +147,7 @@ CREATE TABLE `collaborator` (
 
 LOCK TABLES `collaborator` WRITE;
 /*!40000 ALTER TABLE `collaborator` DISABLE KEYS */;
+INSERT INTO `collaborator` VALUES (1,'company','Umbrella Corporation','666, Hell Road 66666 Racoon City','alice@umbrellacorporation.mov','06.66.66.06.06'),(3,NULL,'Bailloner Xavier',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `collaborator` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -160,7 +160,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:10:29
+-- Dump completed on 2022-06-29 15:45:04
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -214,7 +214,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:10:19
+-- Dump completed on 2022-06-29 15:45:00
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -248,8 +248,9 @@ CREATE TABLE `project` (
   `debut_date` date DEFAULT NULL,
   `estimated_deadline` date DEFAULT NULL,
   `sector` enum('Banque','Assurance','Mutuelles/Retraites','Telecoms','Multimedia','Retail','Defense','Aeronautique','Spatial','Automobile/Transport') DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -258,6 +259,7 @@ CREATE TABLE `project` (
 
 LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` VALUES (2,'https://github.com/WildCodeSchool/2022-03-JS-Toulouse-project-2-veloloco',99,'Trouver une station de vélo, accéder à ses informations, Planifier un itinéraire et avoir des informations sur le trajet','2022-04-03','2022-05-02','Automobile/Transport','VeloLoco');
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -270,8 +272,62 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:42:15
+-- Dump completed on 2022-06-29 15:45:13
 
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demioctet
+-- ------------------------------------------------------
+-- Server version	8.0.28-0ubuntu0.20.04.3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `task` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  `project_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `project_id` (`project_id`),
+  CONSTRAINT `task_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `task`
+--
+
+LOCK TABLES `task` WRITE;
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-29 15:45:17
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -322,7 +378,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:10:23
+-- Dump completed on 2022-06-29 15:45:15
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -376,7 +432,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:10:25
+-- Dump completed on 2022-06-29 15:45:19
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -410,8 +466,9 @@ CREATE TABLE `user` (
   `phone_number` varchar(32) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
   `github_address` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -432,7 +489,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:10:34
+-- Dump completed on 2022-06-29 15:45:11
 
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
@@ -486,4 +543,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-29 13:10:27
+-- Dump completed on 2022-06-29 15:45:02
