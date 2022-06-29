06831bf4465b18216df009ed1f4a7ad51bcf1e9c
MidNightRoad <bergerpierre09@gmail.com>
2022-06-29 15:05:35 +0200

    add conenction page

--- frontend/src/pages/Connection.jsx
+++ frontend/src/pages/Connection.jsx
@@ -1,10 +1,59 @@
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
+import "../assets/css/connection.css";
+import { useForm } from "react-hook-form";
+import perso from "../assets/image/perso1.png";
+import logo from "../assets/image/logo.png";
 
 function Connection() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="homecontainer">
-      <div className="leftside">coucou</div>
-      <div className="rightside"> lol</div>
+      <div className="centralcard">
+        <div className="leftside">
+          <h1 className="titlepresentation">
+            Bienvenue sur <span className="colortextchange"> Unknow </span>
+          </h1>
+          <p className="textpresentation">
+            La plateforme de management projet de{" "}
+            <span className="colortextchange"> Apside </span>
+          </p>
+          <img src={perso} alt="perso" className="imgperso" />
+        </div>
+        <div className="rightside">
+          <img src={logo} alt="logo" className="logo" />
+          <h1 className="title">Connexion</h1>
+
+          <form className="formconnection" onSubmit={handleSubmit(onSubmit)}>
+            <input
+              className="inputform"
+              placeholder="Email"
+              autoComplete="off"
+              {...register("email", { required: true })}
+            />
+
+            <input
+              className="inputform"
+              placeholder="Password"
+              autoComplete="off"
+              {...register("password", { required: true })}
+            />
+
+            {errors.exampleRequired && <span>This field is required</span>}
+
+            <button type="submit" className="submitbutton">
+              Connexion
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
