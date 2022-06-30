20dbb325a82d97dbe040568cb78fa6e1ed1307f0
Aristotia <antoine.belloni@gmail.com>
2022-06-30 15:06:03 +0200

    github api calls working but limit passed

--- frontend/src/components/ProjectCards.jsx
+++ frontend/src/components/ProjectCards.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function ProjectCards() {
+  const [githubDataCommits, setGitHubDataCommits] = useState([]);
+  const [githubDataProjects, setGithubDataProjects] = useState([]);
+  const [backendProjects, setbackendProjects] = useState([]);
+  // const [githubUser, setGithubUser] = useState("Aristotia");
+
   const placeholder = [
     {
       name: "manger",
@@ -50,17 +56,32 @@ function ProjectCards() {
     "lorem ipsum machin truc",
     "lorem ipsum machin truc",
   ];
-  const placeholderPullRequest = [
-    "Pull request",
-    "Pull request",
-    "Pull request",
-    "Pull request",
-    "Pull request",
-    "Pull request",
-    "Pull request",
-    "Pull request",
-    "Pull request",
-  ];
+
+  // TO DO ADD GITHUBUSER TO URL
+
+  const fetchGithubProjectsData = () => {
+    axios
+      .get(`https://api.github.com/repos/Aristotia/DemiOctet/`)
+      .then((data) => setGithubDataProjects(data.data));
+  };
+
+  const fetchGithubCommitsData = () => {
+    axios
+      .get(`https://api.github.com/repos/Aristotia/DemiOctet/commits`)
+      .then((data) => setGitHubDataCommits(data.data));
+  };
+
+  const fetchBackendProjectsData = () => {
+    axios
+      .get(`http://localhost:5000/projects`)
+      .then((data) => setbackendProjects(data.data));
+  };
+
+  useEffect(() => {
+    fetchGithubProjectsData();
+    fetchGithubCommitsData();
+    fetchBackendProjectsData();
+  }, []);
   return (
     <div className="member-card">
       <div className="todo-list-projects-cards">
@@ -73,19 +94,21 @@ function ProjectCards() {
           ))}
         </div>
       </div>
-      <div className="main-section-projects-cards">
-        <div>Name</div>
-        <div>Agency </div>
-        <div>Description </div>
-        <div>Languages </div>
-        <div>
-          <div className="main-section-div">
-            <div>Status </div>
-            <div>Done </div>
-            <div>Avencement </div>
+      {githubDataProjects ? (
+        <div className="main-section-projects-cards">
+          <div>{backendProjects[0].title}</div>
+          <div>Agency </div>
+          <div>{backendProjects[0].description}</div>
+          <div>Languages </div>
+          <div>
+            <div className="main-section-div">
+              <div>Status </div>
+              <div>Done </div>
+              <div>{backendProjects[0].progress}%</div>
+            </div>
           </div>
         </div>
-      </div>
+      ) : null}
       <div className="message-commit-section-projects-cards">
         Messages
         <div className="message-section">
@@ -95,14 +118,21 @@ function ProjectCards() {
             </div>
           ))}{" "}
         </div>
-        Pull Request
-        <div className="pull-request-section">
-          {placeholderPullRequest.map((pullRequest) => (
-            <div>
-              {pullRequest}
-              <hr />{" "}
-            </div>
-          ))}{" "}
+        Commits
+        <div className="commits-section">
+          {githubDataCommits
+            ? githubDataCommits.map((commit) => (
+                <>
+                  <div>{commit.commit.author.name}</div>
+                  <div>{commit.commit.author.date.split("T")[0]}</div>
+                  <div>
+                    {commit.commit.author.date.split("T")[1].slice(0, 8)}
+                  </div>
+                  <div>{commit.commit.message}</div>
+                  <hr />
+                </>
+              ))
+            : null}{" "}
         </div>
       </div>
     </div>
